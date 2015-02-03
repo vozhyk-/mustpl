7f0aee87f96d055b93e84e4bf1f3463bfd0141bd
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-02-03 23:09:49 +0100

    test.rb: Add library usage example

--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
-load '~/dev/ruby/lastfm+vk/vk-music/vk-music-search.rb'
+load '~/dev/ruby/lastfm-vk/vk-music/vk-music-search.rb'
 require 'mustpl'
+require 'pp'
 
+$u = MuStPl::User.new
+$u.storage["vk"] = MuStPl::VKStreamStorage.new($s)
+
+a = get_audio; nil
+
+m = a.to_mustpl("vk"); nil
+
+m.to_m3u($u, "test.m3u")
