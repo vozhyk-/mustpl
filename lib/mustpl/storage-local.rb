0318ab44c1fc32955f3c1a7eb1689a95efb0476b
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2015-02-06 02:09:58 +0100

    Add forgotten storage-local.rb

---
+++ lib/mustpl/storage-local.rb
@@ -0,0 +1,30 @@
+require 'pathname'
+
+module MuStPl
+  class LocalStreamStorage < StreamStorage
+    attr_accessor :storage_dir, :local_path_option
+
+    def initialize(name, storage_dir, local_path_option)
+      super(name)
+      @storage_dir = Pathname.new storage_dir
+      @local_path_option = local_path_option
+    end
+
+    def song_url(song)
+      (@storage_dir + song_path(song)).to_s
+    end
+
+    def path_exist?(path)
+      (@storage_dir + path).exist?
+    end
+
+    def song_path(song)
+      song[@local_path_option]
+    end
+
+    # def play(song, player)
+    #   full_path = @storage_dir + song[@local_path_option]
+    #   player.add_to_playlist(full_path)
+    # end
+  end
+end
